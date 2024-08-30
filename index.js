581105535ad03604b7a2ca713aa704db8873b5c5
prabath1998 <prabhathudayanga1998@gmail.com>
2024-08-30 22:41:43 +0530

    use cookie parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js"
 
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000
 app.use(express.json());
+app.use(cookieParser())
 
 app.use('/api/auth',authRoutes)
 
