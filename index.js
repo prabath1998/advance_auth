8fe695166680eb94549c51fddb3a47a481a1354d
prabath1998 <prabhathudayanga1998@gmail.com>
2024-08-31 20:57:08 +0530

    fix minor bug

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ import authRoutes from "./routes/auth.route.js"
 
 dotenv.config();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 const app = express();
 const PORT = process.env.PORT || 5000
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser())
 
